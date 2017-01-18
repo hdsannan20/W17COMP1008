eb31b83329ca4bcd8678ab60e4d0acb416eeeb31
hdsan <hdsan@10.250.15.220>
2017-01-18 12:45:00 -0500

    added card and deck of cards classes

---
+++ src/january18/Card.java
@@ -0,0 +1,62 @@
+package january18;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hdsan
+ */
+public class Card {
+    private String faceName, suit;
+    private int faceValue;
+    public Card(String face, String suit, int value)
+    {
+        faceName = face;
+        setSuit(suit);
+        setValue(value);
+        
+    }
+    // this method all validate if the null is valid 
+        private void setSuit(String suit) {
+            if (suit.equalsIgnoreCase("heart") || suit.equalsIgnoreCase("diamonds") || suit.equalsIgnoreCase("spades") || suit.equalsIgnoreCase("club"))
+            {
+                this.suit=suit;
+
+            }
+            else 
+                  throw new IllegalArgumentException("suit");
+            
+    }
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+        //this will alidate the face value is between 2 to 14 
+        private void setValue(int value) {
+            if (value <2 || value>14) {
+              
+                throw new IllegalArgumentException("Face value must be 2-4 inclusive");
+            }
+            else 
+            {
+                faceValue = value;
+            }
+                        
+        }
+        public String toString() {
+            return faceName + " of " +suit;
+        }
+}
+
---
+++ src/january18/DeckOfCards.java
@@ -0,0 +1,61 @@
+package january18;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hdsan
+ */
+public class DeckOfCards {
+    ArrayList<Card> deck;
+    
+    public DeckOfCards() 
+    {
+        String[] suits = {"hearts", "diamonds", "spades", "clubs"};
+        String[] faceName = {"two", "three", "four", "five", "six", "seven", "eight","nine", "ten", "eleven", "twelve", "thirteen", "fourteen"}; 
+               
+        
+        for (String suit : suits) 
+        
+            for(int i=0;i<faceName.length;i++)
+            {
+                  Card newCard = new Card( suit, faceName[i],i+2 );
+            deck.add(newCard);
+            }
+        
+    }
+          public  String toString()
+          {
+              String deckString ="";
+              for (Card card: deck)
+              {
+                deckString += card.toString() + "%n";
+              }
+              return deckString;
+          }
+            // this method will shufle deck of card
+          public void shuffle()
+          {
+              Collections.shuffle(deck);
+          }
+          //this method will deal the top of card 
+          public Card dealTopCard()
+          {
+              if (deck.isEmpty())
+                  return null;
+              else
+                  return deck.remove(0);
+          }
+          
+        
+}
+
+
