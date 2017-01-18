584d1e4980bf46e66c4781970ada390bb012084c
hdsan <hdsan@10.250.15.220>
2017-01-18 12:24:27 -0500

    The Initial submission

---
+++ src/january18/January18.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package january18;
+
+/**
+ *
+ * @author hdsan
+ */
+public class January18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("Hello github world");
+    }
+    
+}
