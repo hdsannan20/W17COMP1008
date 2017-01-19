f3e6c270010f6a0114c4d1299d63330a802fd34c
hdsan <hdsan@10.250.15.220>
2017-01-19 10:05:39 -0500

    added game of war class

---
+++ src/january18/GameOfWar.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package january18;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hdsan
+ */
+public class GameOfWar {
+   private ArrayList<Card> p1Hand;
+    private ArrayList<Card> p2Hand;
+    
+    public GameOfWar()
+    {
+        p1Hand = new ArrayList<>();
+        p2Hand = new ArrayList<>();
+        
+        DeckOfCards theDeck = new DeckOfCards();
+        theDeck.shuffle();
+        
+        dealTheCards(theDeck);
+    }// end of the constructor
+    /**
+     * this method will 'deal' all of the cards to the players
+     */
+    private void dealTheCards(DeckOfCards theDeck)
+    {
+        for (int cardNum=0; cardNum < 52; cardNum++) 
+        {
+            if (cardNum % 2 == 0)
+                p1Hand.add(theDeck.dealTopCard());
+            else 
+                p2Hand.add(theDeck.dealTopCard());
+        }
+    }
+    /**
+     * this method will stimulate playing the game called 'war'.
+     */
+    public void playTheGame()
+            {
+               while (!p1Hand.isEmpty() && !p2Hand.isEmpty()) 
+               {
+                   playHand();
+                   
+               }
+               if (p1Hand.isEmpty())
+                   System.out.println("Player 2 is the winner!");
+               else
+                   System.out.println("Player 1 is the winner!");
+                
+                
+            }
+    /**
+     * this method will stimulate playing 3 hand at the game of the war 
+     */
+    private void playHand()
+    {
+        Card p1Card = p1Hand.remove(0);
+        Card p2Card = p2Hand.remove(0);
+        
+        System.out.printf("Player 1: %s cards in hand: %d", p1Card, p1Hand.size()+1);
+        System.out.printf("\tPlayer 2: %s cards in hand: %d%n", p2Card, p2Hand.size()+1);
+        
+        if (p1Card.getFaceValue() > p2Card.getFaceValue())
+        {
+            p1Hand.add(p1Card);
+            p2Hand.add(p2Card);
+        }
+        
+        // player 2's card is higher then player 1's 
+        else if (p2Card.getFaceValue() > p1Card.getFaceValue())
+        {
+            p2Hand.add(p1Card);
+            p2Hand.add(p2Card);
+        }
+        
+        //otherwise its "WAR"
+        else 
+        {
+            ArrayList<Card> warPile = new ArrayList<>();
+            warPile.add(p1Card);
+            warPile.add(p2Card);
+            playWarHand(warPile);
+        } // end of method playHand() 
+    }
+    /**
+     * this method will stimulate playing the war hand
+     */
+    private void playWarHand(ArrayList<Card> warPile)
+    {
+         // check if player 1 had enough cards
+        if (p1Hand.size() <3)
+        {
+            p2Hand.addAll(p1Hand);
+            p1Hand.clear();
+            p2Hand.addAll(warPile);
+            return;
+        }
+        // check if player 2 had enough cards
+        if (p2Hand.size() <3)
+        {
+            p1Hand.addAll(p2Hand);
+            p2Hand.clear();
+            p1Hand.addAll(warPile);
+            return;
+        }
+        
+        warPile.add(p1Hand.remove(0));
+        warPile.add(p1Hand.remove(0));
+        warPile.add(p2Hand.remove(0));
+        warPile.add(p2Hand.remove(0));
+        
+        Card p1Card = p1Hand.remove(0);
+        Card p2Card = p1Hand.remove(0);
+        
+        //if player 1 wins
+        if (p1Card.getFaceValue() > p2Card.getFaceValue())
+        {
+            p1Hand.addAll(warPile);
+            p1Hand.add(p1Card);
+            p1Hand.add(p1Card);
+        }
+        //if player 2 wins
+        else if (p2Card.getFaceValue() > p1Card.getFaceValue())
+          {
+            p2Hand.addAll(warPile);
+            p2Hand.add(p1Card);
+            p2Hand.add(p2Card);
+        }  
+        else
+        {
+            warPile.add(p1Card);
+            warPile.add(p2Card);
+            this.playWarHand(warPile);
+        }
+        
+    }
+            
+}
